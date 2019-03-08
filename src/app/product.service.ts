66e3d02cd79a48f5c994ad925daf34ae26ba9ee7
Andrew Belonger <abelonger@jjkeller.com>
2019-03-08 10:50:38 -0600

    step 3 completed

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): Observable<Response> {
+    return this._http.get(this._albumUrl).pipe(
+      map((response) => response.json())
+    );
+  }
+
+}
