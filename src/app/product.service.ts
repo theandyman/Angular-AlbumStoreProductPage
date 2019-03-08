6cb6c6fb4d31005cfe5b859dbeef07a07326d5d9
Andrew Belonger <abelonger@jjkeller.com>
2019-03-08 10:55:38 -0600

    step 3 fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 
 @Injectable()
@@ -9,7 +8,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<Response> {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).pipe(
       map((response) => response.json())
     );
