be3622cfd7614c974e7f9fcc0ebd6f6e3f96733a
Andrew Belonger <abelonger@jjkeller.com>
2019-03-08 15:16:30 -0600

    step 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductService } from './product.service';
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductService } from './product.service';
+
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
