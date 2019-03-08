96bd51b5931b108b85a16ef05bb4e9d29b690ab3
Andrew Belonger <abelonger@jjkeller.com>
2019-03-08 15:06:10 -0600

    step 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -15,4 +17,8 @@ export class ProductService {
     map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).
+    map((response) => <Product[]>response.json());
+  }
 }
